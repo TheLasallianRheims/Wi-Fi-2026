3520576a6e65e20be279dd247d62e08431ed54a2
Infa-fullstack <infawastaken@gmail.com>
2026-08-18 01:15:36 +0800

    Pinoystagram mobile homepage to be linked with the other web pages when I am done with Pinoystagram and its entirety.

---
+++ index.js
@@ -0,0 +1,13 @@
+(function () {
+    document.querySelector('.header__menu').addEventListener('click', function () {
+        alert('Menu coming soon!');
+    });
+
+    document.querySelector('.header__search').addEventListener('click', function () {
+        alert('Search coming soon!');
+    });
+
+    document.querySelector('.carousel-chevron').addEventListener('click', function () {
+        alert('Next page coming soon!');
+    });
+})();
